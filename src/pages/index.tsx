3549812b13de555fb344a2da1ed34c686a997248
andsonder <changlu@keter.top>
2022-02-24 08:30:02 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
               <div className={styles.buttons}>
                   <Link
                       className="button button--secondary button--lg"
-                      to="/docs/deep-learning/基础知识/深度学习基础知识目录">
+                      to="/docs/deep-learning/基础知识/1-深度学习基础知识目录.md">
                   </Link>
               </div>
           </div>
