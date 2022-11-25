c308ce371b56ca93203ca1b2a4b90f7efefa5fe0
andsonder <changlu@keter.top>
2022-11-25 09:46:43 +0800

    whatever

--- a.sh
+++ a.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# git filter-branch -f --env-filter '
-# if [ "$GIT_AUTHOR_NAME" = "coronapolvo" ]
-# then
-# export GIT_AUTHOR_NAME="andsonder"
-# export GIT_AUTHOR_EMAIL="changlu@keter.top"
-# fi
-# ' HEAD
-
-git filter-branch -f --env-filter '
-if [ "$GIT_COMMITTER_NAME" = "coronapolvo" ]
-then
-export GIT_COMMITTER_NAME="andsonder"
-export GIT_COMMITTER_EMAIL="changlu@keter.top"
-fi
-' HEAD
+git filter-branch -f --env-filter "
+GIT_AUTHOR_NAME='andsonder';
+GIT_AUTHOR_EMAIL='changlu@keter.top';
+GIT_COMMITTER_NAME='andsonder';
+GIT_COMMITTER_EMAIL='changlu@keter.top'
+" HEAD
