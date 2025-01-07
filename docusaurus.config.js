e9c70161a049ed0c8f3d6d833ceff0c9340d656d
andsonder <changlu@keter.top>
2025-01-08 00:00:46 +0800

    [misc] update sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
                         position: "left",
                     },
                     {
-                        to: "/docs/high_performance/分布式/分布式基础知识/分布式训练",
+                        to: "/docs/high_performance/分布式基础知识/分布式训练",
                         activeBasePath: '/docs/high_performance',
                         label: "HPC&DS",
                         position: "left",
